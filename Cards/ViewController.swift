08193b008710f4b729bd7e9e6083edc9f585f355
Dougly <dgalante11@gmail.com>
2016-09-23 17:14:33 -0400

    cards update when button is pressed

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // TODO: Create IB outlets
+    
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    @IBOutlet weak var middle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func show3OfSpades(_ sender: AnyObject) {
+        displayCard(suit: "♠️", number: "3")
+    }
     
+    @IBAction func showFourOfClubs(_ sender: AnyObject) {
+        displayCard(suit: "♣️", number: "4")
+    }
     
-    // TODO: IB actions and code to update UI
-
+    @IBAction func showEightOfDiamonds (_ sender: AnyObject) {
+        displayCard(suit: "♦️", number: "8")
+    }
+    
+    @IBAction func showTenOfHearts (_ sender: AnyObject) {
+        displayCard(suit: "♥️", number: "10")
+    }
+    
+    func displayCard (suit: String, number: String) {
+        topLeft.text = suit
+        bottomRight.text = suit
+        middle.text = number
+    }
   
 
 }
